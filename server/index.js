a0f6e6fe1b198f2b66fdb38cc9d97fde898aec67
Owen Watkins <o.watkins@protonmail.com>
2019-08-31 13:51:23 -0400

    Changed listen port

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '~/../src/index.html'));
 });
 
-var server = app.listen(8081, function() {
+var server = app.listen(8080, function() {
 	var port = server.address().port;
 	var host = server.address().address;
 	console.log('Express running at http://%s:%s', host, port);
